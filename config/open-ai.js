a00a1a725b2c7818e35d7bc83518dfc0ff2af966
pahmel0 <mantasx5@gmail.com>
2024-03-13 22:32:43 +0100

    Update dependencies and API key configuration

--- config/open-ai.js
+++ config/open-ai.js
@@ -1,12 +1,21 @@
-import { Configuration, OpenAIApi } from 'openai';
+// import { Configuration, OpenAIApi } from 'openai';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+// console.log("API Key:", process.env.OPENAI_API_KEY);
+
+// export default openai;
+
+import { OpenAI } from 'openai';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
 console.log("API Key:", process.env.OPENAI_API_KEY);
 
 export default openai;
